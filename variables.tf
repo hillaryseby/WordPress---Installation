f1f1d6d2bf44ae13766654c01ca95d5b849147ce
hillary <hillaryseby@gmail.com>
2024-05-07 15:50:53 -0400

    Init

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instance_key_name" {}
+variable "ssh-user" {}
+variable "private_key_path" {}
+variable "instance-type" {}
+variable "instance-ami" {}
+variable "region" {}
+variable "install_type" {}
+variable "mysql_root_password" {}
